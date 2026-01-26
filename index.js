8cac92cc8021a3e05eb08a7607440cdf70d1f4eb
AzharRashidKhan <Azharrashidkhan04@gmail.com>
2026-01-26 11:47:15 +0530

    Use env variables for Firebase

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const cron = require("node-cron");
 const cors = require("cors");
 const admin = require("firebase-admin");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// 🔥 Firebase Admin Setup
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore();
